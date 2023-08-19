de4196404d5009ea9a9046f730836d2a060f2743
Richard Lee <ruli3956@uni.sydney.edu.au>
2023-08-20 00:59:29 +1000

    - Initial Commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: "https://github.com/Web-Building-Blocks/blocks",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/Web-Building-Blocks/blocks",
+  footer: {
+    text: "Nextra Docs Template",
+  },
+};
+
+export default config;
