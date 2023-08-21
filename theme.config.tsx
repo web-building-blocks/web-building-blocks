d7225d5dfe1b9eb41771dd6d0a53def3076808dd
Richard Lee <richard_875@me.com>
2023-08-21 23:21:45 +1000

    - Change project title

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Blocks</span>,
   project: {
     link: "https://github.com/Web-Building-Blocks/blocks",
-  },
-  chat: {
-    link: "https://discord.com",
   },
   docsRepositoryBase: "https://github.com/Web-Building-Blocks/blocks",
   footer: {
