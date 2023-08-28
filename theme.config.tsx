51c483ac2de4bbfaa3e7966b4d0eadbd64e228a9
Richard Lee <richard_875@me.com>
2023-08-28 16:20:35 +1000

    - Add favicon and refactor

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 import Logo from "components/logo";
+import Seo from "components/seo";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
@@ -13,24 +14,7 @@ const config: DocsThemeConfig = {
     return { titleTemplate: "%s - USYD Blocks" };
   },
   docsRepositoryBase: "https://github.com/Web-Building-Blocks/blocks",
-  head: (
-    <>
-      <meta name="og:title" content="Blocks - University of Sydney" />
-      <meta
-        name="og:description"
-        content="Blocks | University of Sydney Design System | Capstone Project COMP5703"
-      />
-      <meta
-        name="og:keywords"
-        content="Blocks,Design,UI,University,Sydney,Software,Engineering"
-      />
-      <meta name="og:robots" content="index, follow" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="og:language" content="English" />
-      <meta name="og:revisit-after" content="50 days" />
-      <meta name="og:author" content="Richard Lee" />
-    </>
-  ),
+  head: <Seo />,
   footer: {
     text: "Blocks - University of Sydney",
   },
