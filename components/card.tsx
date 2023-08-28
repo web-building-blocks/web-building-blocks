883927e51f08d384aef2a7c003d253b8d4188333
Richard Lee <richard_875@me.com>
2023-08-28 14:20:04 +1000

    - Add card component

--- components/card.tsx
+++ components/card.tsx
@@ -5,7 +5,7 @@ export default function Card() {
   return (
     <Tabs defaultValue="desktop" className="mt-5 w-full rounded-md border">
       <div className="h-16 flex items-center justify-center border-b">
-        <TabsList className="grid grid-cols-2 w-40">
+        <TabsList className="grid grid-cols-2 w-96">
           <TabsTrigger value="desktop">Desktop</TabsTrigger>
           <TabsTrigger value="mobile">Mobile</TabsTrigger>
         </TabsList>
@@ -13,15 +13,18 @@ export default function Card() {
       <div className="flex-grow w-full h-full py-20">
         <TabsContent
           value="desktop"
-          className="flex items-center justify-center !w-full !h-full"
+          className="!m-0 flex items-center justify-center !w-full !h-full"
         >
-          <div className="w-20 h-40 bg-blue-500"></div>
+          <iframe
+            className="w-[280px] h-[160px]"
+            src="/components/card/index.html"
+          />
         </TabsContent>
         <TabsContent
           value="mobile"
           className="flex items-center justify-center !w-full !h-full"
         >
-          <div className="w-20 h-48 bg-orange-500"></div>
+          <div className="w-56 h-56 bg-orange-500"></div>
         </TabsContent>
       </div>
     </Tabs>
