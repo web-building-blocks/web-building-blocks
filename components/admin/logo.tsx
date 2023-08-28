23630fd56e04b05139d172224875816f2b86173e
Richard Lee <richard_875@me.com>
2023-08-28 23:00:40 +1000

    - Modify Logo block

--- components/admin/logo.tsx
+++ components/admin/logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import usydLogo from "../../public/static/usyd_logo.png";
+import usydLogo from "../../public/static/usyd_logo.svg";
 
 function Logo() {
   return (
@@ -8,7 +8,7 @@ function Logo() {
       <Image
         src={usydLogo}
         alt="The University of Sydney"
-        className="h-10 w-10 mr-3"
+        className="w-28 mr-3 border-r pr-3"
       />
       <span>Blocks Design System</span>
     </div>
--- theme.config.tsx
+++ theme.config.tsx
@@ -7,18 +7,15 @@ import Seo from "@/src/admin/seo";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
-  project: {
-    link: "https://github.com/Web-Building-Blocks/blocks",
-  },
+  project: { link: "https://github.com/Web-Building-Blocks/blocks" },
   useNextSeoProps() {
     return { titleTemplate: "%s - USYD Blocks" };
   },
   docsRepositoryBase: "https://github.com/Web-Building-Blocks/blocks",
   head: <Seo />,
   darkMode: false,
-  footer: {
-    text: "Blocks - University of Sydney",
-  },
+  nextThemes: { defaultTheme: "light" },
+  footer: { text: "Blocks - The University of Sydney" },
   primaryHue: { dark: 10, light: 10 },
 };
 
