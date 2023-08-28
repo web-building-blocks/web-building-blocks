58cdcd7fc0b6f06b6b09c3dae5e33d6230c10e59
Richard Lee <richard_875@me.com>
2023-08-28 22:35:26 +1000

    - Add template desktop code syntax highlighting

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,33 +1,63 @@
 import React from "react";
+import { Highlight, themes } from "prism-react-renderer";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+const WIDTH_DESKTOP = "280px";
+const HEIGHT_DESKTOP = "160px";
+const WIDTH_MOBILE = "100%";
+const HEIGHT_MOBILE = "160px";
 const DESKTOP_PATH = "/components/card/normal/desktop/";
 const MOBILE_PATH = "/components/card/normal/mobile/";
 const TABS_CTNT_CLASS = "!m-0 flex items-center justify-center !w-full !h-full";
 const CODE_TAB_CLASS =
   "relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none";
 
+const CodeBlock = ({ code, language }: { code: string; language: string }) => (
+  <Highlight theme={themes.github} code={code} language={language}>
+    {({ className, style, tokens, getLineProps, getTokenProps }) => (
+      <pre style={style}>
+        {tokens.map((line, i) => (
+          <div key={i} {...getLineProps({ line })}>
+            <div
+              className={
+                i === 0 ? "mt-2" : i === tokens.length - 1 ? "mb-2" : ""
+              }
+            >
+              <span className="mr-5">{i + 1}</span>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token })} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </pre>
+    )}
+  </Highlight>
+);
+
 export default function Card() {
-  const [htmlCode, setHtmlCode] = React.useState("");
-  const [cssCode, setCssCode] = React.useState("");
+  const [htmlDesktop, setHtmlDesktop] = React.useState<string | null>(null);
+  const [cssDesktop, setCssDesktop] = React.useState<string | null>(null);
+  const [jsDesktop, setJsDesktop] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     fetchHtmlContent();
     fetchCssContent();
+    fetchJsContent();
   }, []);
 
-  const fetchHtmlContent = async () => {
-    const path = `${DESKTOP_PATH}index.html`;
-    const htmlContent = await fetch(path).then((res) => res.text());
-    setHtmlCode(htmlContent);
-  };
+  const fetchHtmlContent = async () =>
+    setHtmlDesktop(await fetchContent(`${DESKTOP_PATH}index.html`));
 
-  const fetchCssContent = async () => {
-    const path = `${DESKTOP_PATH}styles.css`;
-    const cssContent = await fetch(path).then((res) => res.text());
-    setCssCode(cssContent);
-  };
+  const fetchCssContent = async () =>
+    setCssDesktop(await fetchContent(`${DESKTOP_PATH}styles.css`));
+
+  const fetchJsContent = async () =>
+    setJsDesktop(await fetchContent(`${DESKTOP_PATH}index.js`));
+
+  const fetchContent = async (path: string) =>
+    await fetch(path).then((res) => (res.status === 200 ? res.text() : null));
 
   return (
     <Tabs defaultValue="desktop" className="mt-5 w-full">
@@ -41,13 +71,13 @@ export default function Card() {
         <div className="flex-grow w-full h-full py-20 px-14">
           <TabsContent value="desktop" className={TABS_CTNT_CLASS}>
             <iframe
-              className="w-[280px] h-[160px]"
+              className={`w-[${WIDTH_DESKTOP}] h-[${HEIGHT_DESKTOP}]`}
               src={`${DESKTOP_PATH}index.html`}
             />
           </TabsContent>
           <TabsContent value="mobile" className={TABS_CTNT_CLASS}>
             <iframe
-              className="w-[100%] h-[160px]"
+              className={`w-[${WIDTH_MOBILE}] h-[${HEIGHT_MOBILE}]`}
               src={`${MOBILE_PATH}index.html`}
             />
           </TabsContent>
@@ -63,20 +93,38 @@ export default function Card() {
         <Tabs defaultValue="html" className="w-full">
           <div className="flex items-center justify-between pb-3">
             <TabsList className="w-full justify-start rounded-none border-b bg-transparent p-0">
-              <TabsTrigger value="html" className={CODE_TAB_CLASS}>
-                HTML
-              </TabsTrigger>
-              <TabsTrigger value="css" className={CODE_TAB_CLASS}>
-                CSS
-              </TabsTrigger>
-              <TabsTrigger value="js" className={CODE_TAB_CLASS}>
-                JavaScript
-              </TabsTrigger>
+              {htmlDesktop && (
+                <TabsTrigger value="html" className={CODE_TAB_CLASS}>
+                  HTML
+                </TabsTrigger>
+              )}
+              {cssDesktop && (
+                <TabsTrigger value="css" className={CODE_TAB_CLASS}>
+                  CSS
+                </TabsTrigger>
+              )}
+              {jsDesktop && (
+                <TabsTrigger value="js" className={CODE_TAB_CLASS}>
+                  JavaScript
+                </TabsTrigger>
+              )}
             </TabsList>
           </div>
-          <TabsContent value="html">HTML Code</TabsContent>
-          <TabsContent value="css">CSS Code</TabsContent>
-          <TabsContent value="js">JavaScript Code</TabsContent>
+          {htmlDesktop && (
+            <TabsContent value="html">
+              <CodeBlock code={htmlDesktop} language="html" />
+            </TabsContent>
+          )}
+          {cssDesktop && (
+            <TabsContent value="css">
+              <CodeBlock code={cssDesktop} language="css" />
+            </TabsContent>
+          )}
+          {jsDesktop && (
+            <TabsContent value="js">
+              <CodeBlock code={jsDesktop} language="js" />
+            </TabsContent>
+          )}
         </Tabs>
       </TabsContent>
       <TabsContent value="mobile" className={TABS_CTNT_CLASS}>
