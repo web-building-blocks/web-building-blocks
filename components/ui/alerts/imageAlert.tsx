9565ac2f08f599d891256e4b69dc43793c136d35
Richard Lee <richard_875@me.com>
2023-09-04 17:29:10 +1000

    - Update meta file and components files

--- components/ui/alerts/imageAlert.tsx
+++ components/ui/alerts/imageAlert.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Theme from "../layout/theme";
+import Theme from "../../layout/theme";
 
-export default function Textarea() {
+export default function SoundAlert() {
   return (
     <Theme
       widthDesktop="280px"
       heightDesktop="160px"
       widthMobile="100%"
       heightMobile="160px"
-      componentName="textarea"
-      componentStyle="normal"
+      componentName="alerts"
+      componentStyle="image"
     />
   );
 }
--- public/components/alertDialog/sound/desktop/index.js
+++
@@ -1,5 +0,0 @@
-const audio = document.getElementById("audio")
-function showAlertDialog() {
-    audio.play()
-    alert("This a Alert Dialog with sound, click close button to close.")
-}
