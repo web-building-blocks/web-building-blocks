74efd424e97481336d1e4d7fcc87b07de25c4e3b
Richard Lee <richard_875@me.com>
2023-09-04 20:28:57 +1000

    - Update card normal desktop component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -4,12 +4,14 @@ import Theme from "../layout/theme";
 export default function Card() {
   return (
     <Theme
-      widthDesktop="280px"
-      heightDesktop="160px"
+      widthDesktop="450px"
+      heightDesktop="180px"
       widthMobile="100%"
       heightMobile="160px"
       componentName="card"
       componentStyle="normal"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
     />
   );
 }
