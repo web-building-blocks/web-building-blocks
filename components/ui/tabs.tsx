350b94f0fe1cf26924aeece792d82bb4bf1235ab
Vivianvv98 <viviyt98@gmail.com>
2023-09-11 14:55:31 +1000

    Tabs

---
+++ components/ui/tabs.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Theme from "../layout/theme";
+
+export default function Tabs() {
+  return (
+    <Theme
+      widthDesktop="480px"
+      heightDesktop="220px"
+      widthMobile="375px"
+      heightMobile="200px"
+      componentName="tabs"
+      componentStyle="normal"
+      backgroundDesktop="#6abde2"
+      backgroundMobile="#6abde2"
+    />
+  );
+}
