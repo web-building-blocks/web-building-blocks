e334702b1740997abdcddb731ce112da3e78611b
Richard Lee <richard_875@me.com>
2023-09-11 14:56:11 +1000

    - Add CardWithPic and VerticalCardWithPic components
    Modify layout

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Theme from "../layout/theme";
 
-export default function Card() {
+export const Card = () => {
   return (
     <Theme
       widthDesktop="450px"
@@ -14,4 +14,34 @@ export default function Card() {
       backgroundMobile="#1d1d1d"
     />
   );
-}
+};
+
+export const CardWithPic = () => {
+  return (
+    <Theme
+      widthDesktop="250px"
+      heightDesktop="350px"
+      widthMobile="350px"
+      heightMobile="350px"
+      componentName="card"
+      componentStyle="withPic"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
+
+export const VerticalCardWithPic = () => {
+  return (
+    <Theme
+      widthDesktop="600px"
+      heightDesktop="250px"
+      widthMobile="100%"
+      heightMobile="150px"
+      componentName="card"
+      componentStyle="verticalWithPic"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
