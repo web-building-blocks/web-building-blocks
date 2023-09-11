b42a5340699b8a8b530fca7aba1d8d3006da8f5b
Richard Lee <richard_875@me.com>
2023-09-11 19:09:53 +1000

    - Complete dynamic iframe rendering

--- components/layout/theme.tsx
+++ components/layout/theme.tsx
@@ -65,8 +65,12 @@ export default function Theme({
   const MOBILE_PATH = `/components/${componentName}/${componentStyle}/mobile/`;
 
   const desktopIframeRef = React.useRef(null);
+  const mobileIframeRef = React.useRef(null);
   const [widthDesktop, setWidthDesktop] = React.useState<number>(500);
-  const [heightDesktop, setHeightDesktop] = React.useState<number>(500);
+  const [heightDesktop, setHeightDesktop] = React.useState<number>(250);
+  const [heightMobile, setHeightMobile] = React.useState<number>(250);
+
+  const [refresh, setRefresh] = React.useState<number>(0);
 
   const [htmlDesktop, setHtmlDesktop] = React.useState<string | null>(null);
   const [cssDesktop, setCssDesktop] = React.useState<string | null>(null);
@@ -79,7 +83,18 @@ export default function Theme({
     fetchHtmlContent();
     fetchCssContent();
     fetchJsContent();
+    getDesktopIframeBodySize();
+    getMobileIframeBodySize();
   }, []);
+
+  React.useEffect(() => {
+    if (desktopIframeRef.current)
+      desktopIframeRef.current.onload = getDesktopIframeBodySize;
+    if (mobileIframeRef.current)
+      mobileIframeRef.current.onload = getMobileIframeBodySize;
+  }, [refresh]);
+
+  const refreshIframes = () => setRefresh((prev) => prev + 1);
 
   const fetchHtmlContent = async () => {
     setHtmlDesktop(await fetchContent(`${DESKTOP_PATH}index.html`));
@@ -96,35 +111,39 @@ export default function Theme({
     setJsMobile(await fetchContent(`${MOBILE_PATH}index.js`));
   };
 
-  const getIframeBodySize = () => {
+  const getDesktopIframeBodySize = () => {
     if (desktopIframeRef.current) {
       const iframe = desktopIframeRef.current;
       const iframeDocument =
         iframe.contentDocument || iframe.contentWindow.document;
       const iframeBody = iframeDocument.body;
 
-      console.log("here");
       if (iframeBody) {
-        const width = iframeBody.offsetWidth;
-        const height = iframeBody.offsetHeight;
-        setWidthDesktop(width);
-        setHeightDesktop(height);
-        console.log(`Width: ${width}px, Height: ${height}px`);
+        setWidthDesktop(iframeBody.offsetWidth);
+        setHeightDesktop(iframeBody.offsetHeight);
       }
     }
   };
 
-  React.useEffect(() => {
-    if (desktopIframeRef.current) {
-      desktopIframeRef.current.onload = getIframeBodySize;
+  const getMobileIframeBodySize = () => {
+    if (mobileIframeRef.current) {
+      const iframe = mobileIframeRef.current;
+      const iframeDocument =
+        iframe.contentDocument || iframe.contentWindow.document;
+      const iframeBody = iframeDocument.body;
+      if (iframeBody) setHeightMobile(iframeBody.offsetHeight); // Refresh height
     }
-  }, []);
+  };
 
   const fetchContent = async (path: string) =>
     await fetch(path).then((res) => (res.status === 200 ? res.text() : null));
 
   return (
-    <Tabs defaultValue="desktop" className="mt-5 w-full">
+    <Tabs
+      defaultValue="desktop"
+      onValueChange={(e) => refreshIframes()}
+      className="mt-5 w-full"
+    >
       <div className="w-full rounded-md border">
         <div className="h-16 flex items-center justify-center border-b">
           <TabsList className="grid grid-cols-2 w-72 md:w-96">
@@ -152,7 +171,8 @@ export default function Theme({
         >
           <div className={TABS_CTNT_CLASS}>
             <iframe
-              // style={{ width: "400px", height: heightMobile }}
+              ref={mobileIframeRef}
+              style={{ width: "400px", height: heightMobile }}
               src={`${MOBILE_PATH}index.html`}
             />
           </div>
