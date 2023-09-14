14270b727c4cd7ac77b464da7afb05c5405839bd
Herron2048 <whr0624adrian@gmail.com>
2023-09-14 14:48:25 +1000

    add

--- components/ui/links.tsx
+++ components/ui/links.tsx
@@ -2,14 +2,5 @@ import React from "react";
 import Theme from "../layout/theme";
 
 export default function Links() {
-  return (
-    <Theme
-      widthDesktop="280px"
-      heightDesktop="160px"
-      widthMobile="100%"
-      heightMobile="160px"
-      componentName="links"
-      componentStyle="normal"
-    />
-  );
+  return <Theme componentName="links" componentStyle="normal" />;
 }
