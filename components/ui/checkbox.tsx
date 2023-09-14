978efc96e9e8c93197dd723bc9abe92a5a94e986
Herron2048 <whr0624adrian@gmail.com>
2023-09-14 14:33:29 +1000

    add tags,links

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -4,12 +4,21 @@ import Theme from "../layout/theme";
 export default function Checkbox() {
   return (
     <Theme
-      widthDesktop="280px"
-      heightDesktop="160px"
+      // widthDesktop="280px"
+      // heightDesktop="160px"
+      // widthMobile="100%"
+      // heightMobile="160px"
+      // componentName="checkbox"
+      // componentStyle="normal"
+
+      widthDesktop="450px"
+      heightDesktop="180px"
       widthMobile="100%"
-      heightMobile="160px"
+      heightMobile="100px"
       componentName="checkbox"
       componentStyle="normal"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
     />
   );
 }
---
+++ components/ui/tags.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Theme from "../layout/theme";
+
+export default function Tags() {
+  return (
+    <Theme
+      widthDesktop="480px"
+      heightDesktop="260px"
+      widthMobile="100%"
+      heightMobile="160px"
+      componentName="tags"
+      componentStyle="normal"
+    />
+  );
+}
