3141451a9c540ca5c56554808b53f96db9e86170
Chaoyang <1002975372@qq.com>
2023-09-15 21:03:51 +1000

    Alert Component
    - Fix the structure
    - Fix the style
    - Add more sections

--- public/components/alerts/normal/mobile/index.js
+++
@@ -1,8 +0,0 @@
-const alertBox = document.getElementById('alertBox')
-function showAlert() {
-    alertBox.classList.remove('hidden')
-
-    setTimeout(() => {
-        alertBox.classList.add('hidden');
-    }, 3000);
-}
