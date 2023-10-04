6b0ae3030a57c975f43b2002577954ac8ae0647a
BayMax95271 <keliu9527@gmail.com>
2023-10-05 08:13:37 +1100

    Toast - Ke

---
+++ public/components/toast/title/mobile/index.js
@@ -0,0 +1,17 @@
+function showToast() {
+    const toast = document.getElementById("toast");
+    toast.style.opacity = "1";
+    toast.style.visibility = "visible";
+
+    setTimeout(() => {
+        toast.style.opacity = "0";
+        toast.style.visibility = "hidden";
+    }, 3000);
+}
+
+const closeToastBtn = document.getElementById("closeToast");
+closeToastBtn.addEventListener("click", function () {
+    const toast = document.getElementById("toast");
+    toast.style.opacity = "0";
+    toast.style.visibility = "hidden";
+});
