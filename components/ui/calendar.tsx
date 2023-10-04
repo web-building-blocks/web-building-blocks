7c778bc51780a022e928338ee6fd88806c4d1b50
BayMax95271 <keliu9527@gmail.com>
2023-10-05 07:21:57 +1100

    calendar

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Theme from "../layout/theme";
+import { E } from "nextra/dist/types-c8e621b7";
 
 export function Calendar() {
     return <Theme componentName="calendar" componentStyle="normal" />;
@@ -9,6 +10,22 @@ export function DateRangePickerCalendar() {
     return <Theme componentName="calendar" componentStyle="range" />;
 }
 
-export function PureCalendar() {
-    return <Theme componentName="calendar" componentStyle="pure" />;
+export function DateInputCalendar() {
+    return <Theme componentName="calendar" componentStyle="dateInput" />;
+}
+
+export function DateTimeInputCalendar() {
+    return <Theme componentName="calendar" componentStyle="dateTimeInput" />;
+}
+
+export function MonthInputCalendar() {
+    return <Theme componentName="calendar" componentStyle="monthInput" />;
+}
+
+export function TimeInputCalendar() {
+    return <Theme componentName="calendar" componentStyle="timeInput" />;
+}
+
+export function WeekInputCalendar() {
+    return <Theme componentName="calendar" componentStyle="weekInput" />;
 }
