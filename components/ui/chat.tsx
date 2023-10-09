5d0b53333f32310cfa32b84a1c3b04ff2cc7226e
Richard Lee <richard_875@me.com>
2023-10-09 23:41:53 +1100

    - Add chat component mdx page

---
+++ components/ui/chat.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Theme from "../layout/theme";
+
+export const Chat = () => {
+  return (
+    <Theme
+      componentName="chat"
+      componentStyle="normal"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
+
+export const ChatWithImage = () => {
+  return (
+    <Theme
+      componentName="chat"
+      componentStyle="picture"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
+
+export const ChatWithAttachment = () => {
+  return (
+    <Theme
+      componentName="chat"
+      componentStyle="attachment"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
+
+export const ChatWithLink = () => {
+  return (
+    <Theme
+      componentName="chat"
+      componentStyle="link"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
+
+export const ChatFinished = () => {
+  return (
+    <Theme
+      componentName="chat"
+      componentStyle="finish"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
+
+export const ChatList = () => {
+  return (
+    <Theme
+      componentName="chat"
+      componentStyle="list"
+      backgroundDesktop="#1d1d1d"
+      backgroundMobile="#1d1d1d"
+    />
+  );
+};
