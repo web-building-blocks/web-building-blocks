a055dbd2577d607383b3a82ef10896d67ed04505
Richard Lee <richard_875@me.com>
2023-10-25 02:42:39 +1100

    - Add constant strings

---
+++ components/layout/constants/strings.ts
@@ -0,0 +1,6 @@
+export const TABS_WPR_CLASS =
+  "!mt-0 flex-grow w-full h-full py-20 px-5 lg:px-14";
+export const TABS_CTNT_CLASS =
+  "!m-0 flex items-center justify-center !w-full !h-full";
+export const CODE_TAB_CLASS =
+  "relative h-9 rounded-none border-b-2 border-b-transparent bg-transparent px-4 pb-3 pt-2 font-semibold text-muted-foreground shadow-none transition-none data-[state=active]:border-b-primary data-[state=active]:text-foreground data-[state=active]:shadow-none";
