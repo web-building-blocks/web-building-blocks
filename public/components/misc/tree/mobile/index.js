8fbf5af18a8a4a279b9026a63e110799693076eb
BayMax95271 <keliu9527@gmail.com>
2023-11-09 22:52:32 +1100

    Ke - fix

--- public/components/misc/tree/mobile/index.js
+++ public/components/misc/tree/mobile/index.js
@@ -4,11 +4,19 @@ document.querySelectorAll(".toggle").forEach((toggle) => {
         if (children) {
             if (children.style.display === "none") {
                 children.style.display = "block";
-                this.textContent = "[-]";
+                this.classList.add("expanded");
             } else {
                 children.style.display = "none";
-                this.textContent = "[+]";
+                this.classList.remove("expanded");
             }
         }
     });
+});
+
+document.querySelectorAll(".tree-children").forEach((children) => {
+    children.style.display = "none";
+});
+
+document.querySelectorAll(".toggle").forEach((toggle) => {
+    toggle.textContent = "";
 });
