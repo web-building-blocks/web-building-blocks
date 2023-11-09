80811cefea1d7225108ac36c6be9c4c7814d962a
Hsiangwen Cheng <hsiungwang226@gmail.com>
2023-11-09 17:51:21 +1100

    Harrison-components
    Change the style of components

--- public/components/search/keyboardShortcut/desktop/index.js
+++ public/components/search/keyboardShortcut/desktop/index.js
@@ -1,28 +1,22 @@
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   const isCtrlPressed = event.ctrlKey || event.metaKey;
   const isBKeyPressed = event.keyCode === 75;
 
   if (isCtrlPressed && isBKeyPressed) {
-    event.preventDefault(); 
-    searchForm.style.borderColor = "#4f46e5";
-    searchForm.style.borderWidth = '3px';
+    event.preventDefault();
     searchInput.focus();
   }
 });
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
   if (event.target !== searchInput) {
     searchInput.blur();
-    searchForm.style.borderColor = "#dddfe3";
-    searchForm.style.borderWidth = '2px';
   }
 });
 
-searchForm.addEventListener('click', function() {
-    searchForm.style.borderColor = "#4f46e5";
-    searchForm.style.borderWidth = '3px';
-    searchInput.focus();
+searchForm.addEventListener('click', function () {
+  searchInput.focus();
 });
