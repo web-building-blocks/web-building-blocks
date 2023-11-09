191f695310ab7232d0e970d0eb5ecd5fa7756e98
Vivianvv98 <viviyt98@gmail.com>
2023-11-09 15:36:58 +1100

    Finalise the components - Vivian

--- components/ui/breadcrumbs.tsx
+++ components/ui/breadcrumbs.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Theme from "../layout/componentTheme";
 
-export default function Breadcrumbs() {
+export const WithBorder = () => {
   return (
     <Theme
       componentName="breadcrumbs"
       componentStyle="normal"
       backgroundDesktop="#1d1d1d"
       backgroundMobile="#1d1d1d"
+    />
+  );
+}
+
+export const NoBorder = () => {
+  return (
+    <Theme
+      componentName="breadcrumbs"
+      componentStyle="noborder"
+      backgroundDesktop="#fff"
+      backgroundMobile="#fff"
     />
   );
 }
